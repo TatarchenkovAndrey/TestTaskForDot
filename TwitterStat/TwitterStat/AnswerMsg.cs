671d4e0f9537e4767bcd4bfc982ff954e8c713f6
Andrey Tatarchenkov <andrey_t@holdingbp.ru>
2017-10-13 07:40:02 +0000

    Start

---
+++ TwitterStat/TwitterStat/AnswerMsg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace TwitterStat
+{
+    class AnswerMsg
+    {
+        public virtual char Letter { get; set; }
+        public virtual int Count { get; set; }
+
+        public virtual double Rate(int messageValue)
+        {
+            var result= (double) Count / messageValue;
+            return result;
+        }
+    }
+}
---
+++ TwitterStat/TwitterStat/FlockTwits.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterStat
+{
+    class FlockTwits
+    {
+        public virtual Twit[] Tweets { get; set; }
+    }
+}
